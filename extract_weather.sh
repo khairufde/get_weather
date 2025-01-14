8b31be89a8bc4a18a504842718893fca32d218c3
khairufde <khairullahf04@gmail.com>
2025-01-14 23:06:17 +0700

    initial commit

--- extract_weather.sh
+++ extract_weather.sh
@@ -1,28 +1,48 @@
 # Assign city
-city=bandung
+city="Bandung"
 
 # Obtain the weather report
 curl -s wttr.in/$city?T --output weather_report
 
-#To extract Current Temperature
-obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
-echo "The current Temperature of $city: $obs_temp"
+# Extract current temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
 
-# To extract the forecast tempearature for noon tomorrow
-fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
-echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+morning_temp=$(curl -s wttr.in/$city?T | head -13 | tail -1 | grep '°.' | cut -d 'C' -f1 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+noon_temp=$(curl -s wttr.in/$city?T | head -13 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+evening_temp=$(curl -s wttr.in/$city?T | head -13 | tail -1 | grep '°.' | cut -d 'C' -f3 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+night_temp=$(curl -s wttr.in/$city?T | head -13 | tail -1 | grep '°.' | cut -d 'C' -f4 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
 
-#Assign Country and City to variable TZ
-TZ='Indonesia/Bandung'
+tom_morning_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f1 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+tom_noon_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+tom_evening_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f3 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+tom_night_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f4 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
 
+dat_morning_temp=$(curl -s wttr.in/$city?T | head -33 | tail -1 | grep '°.' | cut -d 'C' -f1 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+dat_noon_temp=$(curl -s wttr.in/$city?T | head -33 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+dat_evening_temp=$(curl -s wttr.in/$city?T | head -33 | tail -1 | grep '°.' | cut -d 'C' -f3 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
+dat_night_temp=$(curl -s wttr.in/$city?T | head -33 | tail -1 | grep '°.' | cut -d 'C' -f4 | grep -Eo -e '-?[[:digit:]].*' | grep -Eo '^[0-9]+')
 
-# Use command substitution to store the current day, month, and year in corresponding shell variables:
-hour=$(TZ='Indonesia/Bandung' date -u +%H)
-day=$(TZ='Indonesia/Bandung' date -u +%d)
-month=$(TZ='Indonesia/Bandung' date +%m)
-year=$(TZ='Indonesia/Bandung' date +%Y)
+# Assign timezone
+TZ='Asia/Jakarta'
 
+# Store current date and time
+day=$(TZ=$TZ date +%d)
+month=$(TZ=$TZ date +%m)
+year=$(TZ=$TZ date +%Y)
+
+tom_day=$(TZ=$TZ date --date='1 day' +%d)
+tom_month=$(TZ=$TZ date --date='1 day' +%m)
+tom_year=$(TZ=$TZ date --date='1 day' +%Y)
+
+dat_day=$(TZ=$TZ date --date='2 days' +%d)
+dat_month=$(TZ=$TZ date --date='2 days' +%m)
+dat_year=$(TZ=$TZ date --date='2 days' +%Y)
 
 # Log the weather
-record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp C")
-echo $record>>rx_poc.log
+today_record=$(echo -e "$year,$month,$day,$obs_temp,$morning_temp,$noon_temp,$evening_temp,$night_temp")
+tomorrow_record=$(echo -e "$tom_year,$tom_month,$tom_day,$tom_morning_temp,$tom_noon_temp,$tom_evening_temp,$tom_night_temp")
+dayAfterTomorrow_record=$(echo -e "$dat_year,$dat_month,$dat_day,$dat_morning_temp,$dat_noon_temp,$dat_evening_temp,$dat_night_temp")
+
+echo "$today_record" >> today_weather_data.csv
+echo "$tomorrow_record" >> tomorrow_weather_data.csv
+echo "$dayAfterTomorrow_record" >> dayAfterTomorrow_weather_data.csv
